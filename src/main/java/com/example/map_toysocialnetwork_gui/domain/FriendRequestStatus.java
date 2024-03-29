a95140d9bfbe406159b48776ef457168902ba755
deedeemaan <153390832+deedeemaan@users.noreply.github.com>
2024-03-29 19:03:36 +0200

    Delete src directory

--- src/main/java/com/example/map_toysocialnetwork_gui/domain/FriendRequestStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.map_toysocialnetwork_gui.domain;
-
-public enum FriendRequestStatus {
-    PENDING, APPROVED, REJECTED
-}
--- src/main/java/com/example/map_toysocialnetwork_gui/utils/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.example.map_toysocialnetwork_gui.utils.observer;
-
-import com.example.map_toysocialnetwork_gui.utils.events.Event;
-
-public interface Observer <E extends Event>{
-
-    void update(E e);
-}
