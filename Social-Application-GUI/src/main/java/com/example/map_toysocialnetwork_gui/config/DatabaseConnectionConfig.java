6f64411863f8293576c3fbbc0a9df762a46c02ae
deedeemaan <153390832+deedeemaan@users.noreply.github.com>
2024-03-29 19:48:30 +0200

    Update DatabaseConnectionConfig.java

--- Social-Application-GUI/src/main/java/com/example/map_toysocialnetwork_gui/config/DatabaseConnectionConfig.java
+++ Social-Application-GUI/src/main/java/com/example/map_toysocialnetwork_gui/config/DatabaseConnectionConfig.java
@@ -3,5 +3,5 @@ package com.example.map_toysocialnetwork_gui.config;
 public class DatabaseConnectionConfig {
     public static String DB_CONNECTION_URL = "jdbc:postgresql://localhost:5432/SocialNetwork";
     public static String DB_USER = "postgres";
-    public static String DB_PASSWORD = "Dede2205";
+    public static String DB_PASSWORD = "";
 }
